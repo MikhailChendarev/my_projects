6b7d0b86a55be81b84afd6400d5db1bb96129595
Chendarev Mikhail <mihasya04091986@gmail.com>
2024-08-12 08:57:12 +0300

    first half-working version

--- taskmanagementsystem/src/main/java/com/example/taskmanagementsystem/TaskManagementSystemApplication.java
+++ taskmanagementsystem/src/main/java/com/example/taskmanagementsystem/TaskManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.taskmanagmentsystem.repository")
+@EnableJpaRepositories(basePackages = "com.example.taskmanagementsystem.repository")
 public class TaskManagementSystemApplication {
 
 	public static void main(String[] args) {
--- taskmanagementsystem/src/main/java/com/example/taskmanagementsystem/model/User.java
+++ taskmanagementsystem/src/main/java/com/example/taskmanagementsystem/model/User.java
@@ -1,15 +1,13 @@
 package com.example.taskmanagementsystem.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @Builder
+@Table(name = "\"user\"")
 public class User {
 
     @Id
